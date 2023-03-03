a3e34874b2ce303d22604b171598ba8a3a6a3571
Marcos Rosti <rosti_marcos@hotmail.com>
2023-03-03 16:30:23 -0300

    ocultar VE

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotevn from "dotenv"
+
+dotevn.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
